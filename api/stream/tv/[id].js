3a88a739b8f71e073078c561bbd9d0892e465e95
talhabilgehan <talhabilgehanyilmaz@icloud.com>
2026-09-10 02:12:19 +0300

    Create [id].js

---
+++ api/stream/tv/[id].js
@@ -0,0 +1,40 @@
+export default async function handler(req,res){
+
+const M3U=process.env.M3U_URL;
+
+const txt=await fetch(M3U).then(r=>r.text());
+
+const lines=txt.split("\n");
+
+const id=decodeURIComponent((req.query.id||"").replace("tv-",""));
+
+let info="";
+
+for(let i=0;i<lines.length;i++){
+
+const line=lines[i];
+
+if(line.startsWith("#EXTINF")){
+info=line;
+continue;
+}
+
+if(!line.startsWith("http")) continue;
+
+const name=info.split(",").pop().trim();
+
+if(name===id){
+
+return res.json({
+streams:[
+{url:line.trim()}
+]
+});
+
+}
+
+}
+
+res.json({streams:[]});
+
+}
