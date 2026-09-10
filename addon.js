7cb6f113144022458b055fe39197afa61d1ad2ad
talhabilgehan <talhabilgehanyilmaz@icloud.com>
2026-09-10 17:49:35 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
-const { createCanvas, loadImage } = require("canvas");
+const { createCanvas, loadImage } = require("@napi-rs/canvas");
 
 const {
   getGroups,
