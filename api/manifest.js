8a5d7aeaa3a1993d3247429c493176d9c1c112b9
talhabilgehan <talhabilgehanyilmaz@icloud.com>
2026-09-10 04:19:53 +0300

    Update manifest.js

--- api/manifest.js
+++ api/manifest.js
@@ -1,20 +1,18 @@
 export default function handler(req, res) {
-  res.setHeader("Content-Type","application/manifest+json");
-
   res.status(200).json({
-    id:"talhabilgehan.tata.tv",
-    version:"2.0.0",
-    name:"TATA Live TV",
-    description:"95 Kanallı Nuvio TV",
-    resources:["catalog","stream"],
-    types:["tv"],
-    idPrefixes:["tv-"],
-    catalogs:[
-      {type:"tv",id:"ulusal",name:"Ulusal"},
-      {type:"tv",id:"haber",name:"Haber"},
-      {type:"tv",id:"spor",name:"Spor"},
-      {type:"tv",id:"belgesel",name:"Belgesel"},
-      {type:"tv",id:"cocuk",name:"Çocuk"}
+    id: "tata.live",
+    version: "1.0.0",
+    name: "TATA",
+    description: "Premium Live TV",
+    logo: "https://tata-eosin.vercel.app/logo.png",
+    resources: ["catalog", "stream"],
+    types: ["tv"],
+    catalogs: [
+      { type: "tv", id: "ulusal", name: "Ulusal" },
+      { type: "tv", id: "spor", name: "Spor" },
+      { type: "tv", id: "haber", name: "Haber" },
+      { type: "tv", id: "belgesel", name: "Belgesel" },
+      { type: "tv", id: "cocuk", name: "Çocuk" }
     ]
   });
 }
